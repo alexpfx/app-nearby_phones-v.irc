08907fcb79628e40e23781cfe813ffea7b91d993
alexpfx <alexpfba@gmail.com>
2015-06-29 23:08:33 -0300

    validador rede wifi.

---
+++ app/src/main/java/br/com/alexpfx/irctest/app/NetAddressUtils.java
@@ -0,0 +1,15 @@
+package br.com.alexpfx.irctest.app;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by alexandre on 29/06/15.
+ */
+public class NetAddressUtils {
+
+    public static boolean isValidMACAddress(String line) {
+        Pattern p = Pattern.compile("^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$");
+        final boolean matches = p.matcher(line.toUpperCase()).matches();
+        return matches;
+    }
+}
