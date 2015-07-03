777d7f680a7a550f231ca990d408d5ee0116b62d
alexpfx <alexpfba@gmail.com>
2015-07-03 00:49:34 -0300

    singleton para irc api.

---
+++ app/src/main/java/br/com/alexpfx/irctest/app/irc/IRCApiSingleton.java
@@ -0,0 +1,18 @@
+package br.com.alexpfx.irctest.app.irc;
+
+import com.ircclouds.irc.api.IRCApi;
+import com.ircclouds.irc.api.IRCApiImpl;
+
+/**
+ * Created by alexandre on 02/07/15.
+ */
+public enum IRCApiSingleton {
+    INSTANCE;
+
+    private IRCApi internal = new IRCApiImpl(false);
+
+    public IRCApi get() {
+        return internal;
+    }
+
+}
