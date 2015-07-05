c37c54752e7bfa45196b6b048a99644abf163971
alexpfx <alexpfba@gmail.com>
2015-07-05 01:08:08 -0300

    interactor.

---
+++ app/src/main/java/br/com/alexpfx/irctest/app/irc/IrcInteractor.java
@@ -0,0 +1,11 @@
+package br.com.alexpfx.irctest.app.irc;
+
+/**
+ * Created by alexandre on 04/07/15.
+ */
+public interface IrcInteractor extends IRCMessageService, IRCChannelService, IRCConnectionService {
+
+
+}
+
+
---
+++ app/src/main/java/br/com/alexpfx/irctest/app/mvp/model/interactor/Interactor.java
@@ -0,0 +1,8 @@
+package br.com.alexpfx.irctest.app.mvp.model.interactor;
+
+/**
+ * Created by alexandre on 05/07/15.
+ */
+public interface Interactor {
+    void run ();
+}
