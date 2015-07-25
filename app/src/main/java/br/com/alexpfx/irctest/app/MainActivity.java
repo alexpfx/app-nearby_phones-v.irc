77a0e031437f6d80bc5307f33e2df2a4e6507c4d
alexpfx <alexpfba@gmail.com>
2015-07-24 23:18:05 -0300

    navigation drawer.

--- app/src/main/java/br/com/alexpfx/irctest/app/MainActivity.java
+++ app/src/main/java/br/com/alexpfx/irctest/app/MainActivity.java
@@ -1,14 +1,109 @@
 package br.com.alexpfx.irctest.app;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.navigation_view)
+    NavigationView navigationView;
+
+    @Bind(R.id.viewpager)
+    ViewPager viewPager;
+
+    @Bind(R.id.tabs)
+    TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
+
+        final ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+
+        setupDrawerContent();
+        setupViewPager();
+
+        ButterKnife.bind(this);
+
+    }
+
+    private void setupViewPager() {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        adapter.addFragment(new BlankFragment(), "fragmento exemplo");
+
+        viewPager.setAdapter(adapter);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getGroupId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragments = new ArrayList<>();
+        private final List<String> fragmentTitles = new ArrayList<>();
+
+        public void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            fragmentTitles.add(title);
+        }
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 
 }
